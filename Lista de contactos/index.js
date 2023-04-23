2ef7264f9a24601cebf369ba6dfb0d6593d37779
Andrysc <126534065+Andrysc@users.noreply.github.com>
2023-04-23 12:05:06 -0500

    Add files via upload

---
+++ Lista de contactos/index.js
@@ -0,0 +1,40 @@
+let listaDeContactos = [
+    "juan david",
+    "fabian urrego",
+    "deinson sepoulveda",
+    "daniela ruiz",
+    "luisa villa",
+    "alveiro urrego",
+
+];
+//lista de contactos antes de modificarla
+console.log(listaDeContactos)
+
+function agrgarcontacto(a){
+    let agregar = listaDeContactos.push(a);
+}
+agrgarcontacto ("emilce urrego")
+// agregar contacto
+console.log(listaDeContactos);
+
+function eliminarContactos(){
+    let eliminar = listaDeContactos.shift();
+}
+
+eliminarContactos();
+//lista con un nombre eliminado
+
+console.log(listaDeContactos);
+
+function mostrarContactos(){
+    console.log(mostrarContactos);
+
+}
+mostrarContactos();
+
+
+
+
+
+
+
